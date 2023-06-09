01a1cd31546f27b12153de9e151e70c43d6c2012
vaanya2011 <100355303+vaanya2011@users.noreply.github.com>
2023-06-09 17:50:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,3 +10,46 @@ timer_check = "";
 drawn_sketch = "";
 answer_holder = "";
 score = 0;
+
+function setup(){
+    canvas=createCanvas(300, 300);
+    canvas.center();
+    background("white");
+}
+
+function updateCanvas(){
+    background("white");
+    random_number = Math.floor((Math.random() * quick_draw_data_set.length) + 1);
+console.log(quick_draw_data_set[random_number]);
+sketch = quick_draw_data_set[random_number];
+document.getElementById('sketch_name').innerHTML = 'Sketch To be Drawn: ' + sketch;
+    
+}
+
+function draw(){
+    check_sketch();
+
+    if(drawn_sketch==sketch){
+        answer_holder="set";
+        score++;
+        document.getElementById("score").innerHTML="score:"+score;
+    }
+
+
+}
+
+function check_sketch(){
+    timer_counter++;
+    document.getElementById("timer").innerHTML="timer:"+timer_counter;
+    console.log("timer_counter=" +timer_counter);
+    if(timer_counter>=400){
+        timer_counter=0;
+        timer_check="completed";
+    }
+    if(timer_check=="completed"|| answer_holder=="set"){
+        timer_check=""  ;
+        answer_holder=""  ;
+        updateCanvas();
+
+    }
+}
